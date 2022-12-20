5bf877bfb6d12867c92f8f347e9bee15a1c6c87d
Trang Tung Nguyen <trangtungn@gmail.com>
2022-12-20 13:44:54 +0700

    Add rspec config for devise authentication

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -3,6 +3,19 @@
 require 'rails_helper'
 
 describe 'Posts' do
+  before do
+    user = User.create(
+      email: 'abc@test.co',
+      password: '123456',
+      password_confirmation: '123456',
+      first_name: 'Jon',
+      last_name: 'Doe'
+    )
+    sign_in(user, scope: :user)
+
+    visit new_post_path
+  end
+
   describe 'index' do
     it 'can be reached successfully' do
       visit posts_path
@@ -16,10 +29,6 @@ describe 'Posts' do
   end
 
   describe 'creation' do
-    before do
-      visit new_post_path
-    end
-
     it 'can reach a new form' do
       expect(page.status_code).to eq(200)
     end
@@ -30,7 +39,16 @@ describe 'Posts' do
 
       click_on 'Save'
 
-      expect(page).to have_content(/Some/)
+      expect(page).to have_content(/Some rationale/)
+    end
+
+    it 'can create a new post with a user associated with it' do
+      fill_in 'post[date]', with: Time.zone.today
+      fill_in 'post[rationale]', with: 'User association'
+
+      click_on 'Save'
+
+      expect(User.last.posts.last.rationale).to eq('User association')
     end
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,13 +7,19 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'rspec/rails'
 require 'capybara/rails'
 
+# include Warden::Test::Helpers
+# Warden.test_mode!
+
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
 RSpec.configure do |config|
+  config.include Devise::Test::IntegrationHelpers, type: :feature
+
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
