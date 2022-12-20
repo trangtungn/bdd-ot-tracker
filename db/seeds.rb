06fd8d62075de72e1efd52df5c0ca73bb8b61082
Trang Tung Nguyen <trangtungn@gmail.com>
2022-12-20 13:46:18 +0700

    Update db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create(email: 'test@t.co', password: '123456', password_confirmation: '123456')
+p "User has been created."
+p user
+
+num = 100.freeze
+num.times do |idx|
+  p idx
+  Post.create(user_id: user.id, date: Time.zone.today, rationale: "#{idx} rationale content")
+end
+
+p "#{num} posts have been created."
