f10cc0b90e64f1e15748ff56aff1966acf7e0090
Trang Tung Nguyen <trangtungn@gmail.com>
2022-12-29 16:41:51 +0700

    Update homepage spec

--- spec/features/static_spec.rb
+++ spec/features/static_spec.rb
@@ -7,6 +7,7 @@ describe 'Static' do
     it 'can be reached successfully' do
       visit root_path
       expect(page.status_code).to eq(200)
+      expect(page).to have_content(/Homepage/)
     end
   end
 end
