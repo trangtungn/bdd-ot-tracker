7cd477b7f8f7f9bb071c586f515b7872cc455f7b
Trang Tung Nguyen <trangtungn@gmail.com>
2023-01-13 13:32:47 +0700

    Add validate helper

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,15 @@
+# frozen_string_literal: true
+
 module PostsHelper
+  def validate(post)
+    return unless post.errors.any?
+
+    output = []
+
+    post.errors.full_messages.each do |message|
+      output << "toastr.error(\"#{message}\");"
+    end
+
+    output.join(' ')
+  end
 end
