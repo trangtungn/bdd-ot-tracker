9db10188103905a8a01917cdcd237dbfe3c5f0dc
Trang Tung Nguyen <trangtungn@gmail.com>
2023-01-27 15:19:27 +0700

    Added user reference to posts

--- db/migrate/20221213070547_add_user_id_to_posts.rb
+++ db/migrate/20221213070547_add_user_id_to_posts.rb
@@ -1,5 +1,14 @@
 class AddUserIdToPosts < ActiveRecord::Migration[6.1]
   def change
-    add_reference :posts, :user, null: false, foreign_key: true
+    add_reference :posts, :user, foreign_key: true
+
+    reversible do |direction|
+      direction.up do
+        uid = User.first.id
+        Post.update_all(user_id: uid)
+
+        change_column_null :posts, :user_id, false
+      end
+    end
   end
 end
