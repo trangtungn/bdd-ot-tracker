5303cc0b66cb1ad81051318e172c23a127ac8bf4
Trang Tung Nguyen <trangtungn@gmail.com>
2023-01-30 18:23:32 +0700

    Add status to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 class Post < ApplicationRecord
+  enum status: {
+    submitted: 0,
+    approved: 1,
+    rejected: 2
+  }
+
   belongs_to :user
 
   validates(*%i[date rationale], presence: true)
