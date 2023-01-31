866ecff61896c41906320271aed18270a1620072
Trang Tung Nguyen <trangtungn@gmail.com>
2023-02-01 03:25:30 +0700

    Added post status

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 module PostsHelper
+  COLOR_MAPPER = {
+    'approved' => 'success',
+    'submitted' => 'primary',
+    'rejected' => 'secondary'
+  }.freeze
+
   def validate(post)
     return unless post.errors.any?
 
@@ -11,5 +17,9 @@ module PostsHelper
     end
 
     output.join(' ')
+  end
+
+  def status_span(status)
+    content_tag(:span, status.titleize, class: "badge bg-#{COLOR_MAPPER[status]}")
   end
 end
