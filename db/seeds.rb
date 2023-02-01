58a45ee354f615cbdca851308dc0e3cd627d782f
Trang Tung Nguyen <trangtungn@gmail.com>
2023-02-02 01:29:34 +0700

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,34 @@ user = User.create!(
   password: '123456',
   password_confirmation: '123456'
 )
-p "User has been created."
+p "Test user has been created."
 p user
+
+user2 = User.create!(
+  first_name: 'Test2',
+  last_name: 'T',
+  email: 'test2@t.co',
+  password: '123456',
+  password_confirmation: '123456'
+)
+p "Test2 user has been created."
+p user2
+
+admin = AdminUser.create!(
+  first_name: 'Admin',
+  last_name: 'T',
+  email: 'admin@t.co',
+  password: '123456',
+  password_confirmation: '123456'
+)
+p "Admin user has been created."
+p admin
 
 num = 100.freeze
 num.times do |idx|
   Post.create!(
     user_id: user.id,
+    overtime_request: 1.0,
     date: Time.zone.today,
     rationale: "#{idx} content: There are many variations " \
       "of passages of Lorem Ipsum available, but the majority have " \
