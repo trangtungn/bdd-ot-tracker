b6e4884db3d7a1a7a2605d3f2030e7a4c09c2dfb
Trang Tung Nguyen <trangtungn@gmail.com>
2023-02-03 02:23:05 +0700

    Created sms tool

---
+++ lib/sms_tool.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'twilio-ruby'
+
+module SmsTool
+  def self.send_sms(number:, message:)
+    @client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
+
+    @client.messages.create(
+      from: ENV['TWILIO_PHONE_NUMBER'],
+      to: number,
+      body: message
+    )
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'fake_sms_tool'
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
@@ -7,6 +9,10 @@ RSpec.configure do |config|
 
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
+  end
+
+  config.before do
+    stub_const('SmsTool', FakeSmsTool)
   end
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
