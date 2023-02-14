e1cfb902e346800d20cfc60194f7cbbc8f51a387
Trang Tung Nguyen <trangtungn@gmail.com>
2023-02-14 14:05:57 +0700

    Update migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_03_092109) do
+ActiveRecord::Schema.define(version: 2023_02_14_063354) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,15 @@ ActiveRecord::Schema.define(version: 2023_02_03_092109) do
     t.integer "status", default: 0
     t.decimal "overtime_request", default: "0.0"
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "currency"
+    t.float "price_per_hour"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
