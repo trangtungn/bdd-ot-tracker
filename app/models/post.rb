d96c51a7c6da029fd56d6af535183a1c53bcc231
Trang Tung Nguyen <trangtungn@gmail.com>
2023-03-22 00:16:41 +0700

    Add update audit log callback for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,4 +14,13 @@ class Post < ApplicationRecord
   validates :overtime_request, numericality: { greater_than: 0.0 }
 
   scope :post_by, ->(user) { where(user: user) }
+
+  after_save :update_audit_log
+
+  private
+
+  def update_audit_log
+    audit_log = AuditLog.where(user_id: user_id, start_date: (date - 7.days)..date).last
+    audit_log.confirmed!
+  end
 end
